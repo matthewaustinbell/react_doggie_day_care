19c1e713ed3f7f6ca8c9bc6f28fa175b74899306
Matt Bell <matthewaustinbell@gmail.com>
2019-06-20 19:23:19 -0500

    setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <button className= 'btn btn-danger'>button</button>
       </header>
     </div>
   );
