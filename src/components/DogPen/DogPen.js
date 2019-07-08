004b7e7f86672c64b74d4541bf668274730a974e
Matt Bell <matthewaustinbell@gmail.com>
2019-07-08 14:51:16 -0500

    wip

--- src/components/DogPen/DogPen.js
+++ src/components/DogPen/DogPen.js
@@ -1,20 +1,9 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import './DogPen.scss';
-import dogData from '../../../helpers/data/dogData';
 import Dog from '../Dog/Dog';
 
 class DogPen extends React.Component {
-  state = {
-    dogs: [],
-  }
-
-  componentDidMount() {
-    dogData.getDogs()
-      .then(dogs => this.setState({ dogs }))
-      .catch(err => console.error('no dogs available', err));
-  }
-
   render() {
     const dogComponents = this.state.dogs.map(d => (
       <Dog key={d.id} dog={d} />
